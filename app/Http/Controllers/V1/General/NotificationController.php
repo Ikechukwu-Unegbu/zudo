3ce7ff73b1b1cd8815015bed1d55d2cc76c44dac
ikechukwu-unegbu <iunegbu94@yahoo.com>
2022-10-12 17:00:49 +0100

    M

--- app/Http/Controllers/V1/General/NotificationController.php
+++ app/Http/Controllers/V1/General/NotificationController.php
@@ -3,12 +3,42 @@
 namespace App\Http\Controllers\V1\General;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class NotificationController extends Controller
 {
     public function index(){
         
         return view('admin.notification.index');
+    }
+
+    public function markall(){
+        $user = User::findOrFail(Auth::user()->id);
+        foreach ($user->unreadNotifications as $notification) {
+            $notification->markAsRead();
+        }
+        //return redirect
+        return redirect()->route('user.nofitication', $user->id);
+    }
+
+      
+    public function userNotification($userId){
+        
+        if($userId != Auth::user()->id){
+            return redirect()->route('user.nofitication', [Auth::user()->id]);
+        }
+
+        if(request()->input('filter') == null || request()->input('filter')=='unread'){
+            $notification = User::find($userId)->unreadNotifications;
+            return view('customer.notification.user_notification')->with('notifications', $notification);
+        }
+
+        if(request()->input('filter') == 'all'){
+            $notification = User::find($userId)->notifications()->paginate(30);
+            return view('customer.notification.user_notification')->with('notifications', $notification);
+        }
+        
     }
 }
--- resources/views/customer/home/index.blade.php
+++ resources/views/customer/home/index.blade.php
@@ -2,7 +2,7 @@
 
 
 @section('head')
-<title>Spartan Cooperative</title>
+<title>Zudovest Cooperative</title>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 @endsection
 
